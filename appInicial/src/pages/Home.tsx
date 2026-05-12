0aa88539279677160078a1c43a7a6c785ad80960
ccanutinho01 <ccanutinho01@gmail.com>
2026-05-12 19:27:11 -0300

    atualização do codigo
    Foram feitas alterações no codigo relacionado a home

---
+++ appInicial/src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+      <ion-button expand="block" color="primary">Salvar</ion-button>  
+    </IonPage>
+  );
+};  
+
+export default Home;
