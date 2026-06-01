098c8ecda24a9ab1f6bc3af13e6aa9ae2fa89c45
ccanutinho01 <ccanutinho01@gmail.com>
2026-06-01 19:36:03 -0300

    implementação de um novo banco de dados
    Foram acrescentados novos dados no banco de dados

---
+++ appInicial/backend/server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const arquivo = "produtos.json";
+app.get("/produtos", (req, res) => {
+    const dados = fs.readFileSync(arquivo);
+    const produtos = JSON.parse(dados);
+    res.json(produtos);
+});
+
+app.post("/produtos", (req, res) => {
+    const dados = fs.readFileSync(arquivo);
+    const produtos = JSON.parse(dados);
+
+    const novoProduto = {
+        id: Date.now(),
+        ...req.body
+    };
+
+    produtos.push(novoProduto);
+
+    fs.writeFileSync(arquivo, JSON.stringify(produtos, null, 2));
+
+    res.json(novoProduto);
+});
+
+app.put("/produtos/:id", (req, res) => {
+    const dados = fs.readFileSync(arquivo);
+    let produtos = JSON.parse(dados);
+
+    produtos = produtos.map(p => 
+        p.id == req.params.id ? { ...p, ...req.body } : p
+    );
+
+    fs.writeFileSync(arquivo, JSON.stringify(produtos, null, 2));
+
+    res.json({ msg: "Atualizado" });
+});
+
+app.delete("/produtos/:id", (req, res) => {
+    const dados = fs.readFileSync(arquivo);
+    let produtos = JSON.parse(dados);
+
+    produtos = produtos.filter(p => p.id != req.params.id);
+
+    fs.writeFileSync(arquivo, JSON.stringify(produtos, null, 2));
+
+    res.json({ msg: "Removido" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
